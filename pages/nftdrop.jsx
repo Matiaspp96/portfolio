86ea408b28ba52d89445c5317fd0f82a8fe6318d
Matiaspp96 <matiaas.p@gmail.com>
2022-08-01 20:06:17 -0300

    feat:(nftDrop)

---
+++ pages/nftdrop.jsx
@@ -0,0 +1,97 @@
+import Image from 'next/image';
+import {
+	SiTypescript,
+	SiEthereum,
+	SiTailwindcss,
+	SiNextdotjs,
+    SiVercel,
+} from 'react-icons/si';
+import NFTDrop from '../public/assets/Render NFT Drop.png';
+
+const nftdrop = (props) => {
+	const {projects} = props
+	return (
+		<div className='w-full'>
+			<div className='w-full h-full pt-20 relative flex flex-col items-center'>
+				<div className='flex flex-col items-center w-full bg-black/5 rounded-xl py-4 '>
+					<Image
+						className='opacity-80 transform hover:opacity-100 transition ease-in duration-200'
+						src={NFTDrop}
+						width={750}
+						height={400}
+						alt='/'
+					/>
+					<div className='text-center'>
+						<h2 className='uppercase text-2xl text-center tracking-widest text-[#516ce5]'>
+                        NFT Drop
+						</h2>
+						<h3>Next.js | ThirdWeb | Sanity</h3>
+					</div>
+				</div>
+				<div className='max-w-[1240px] mx-5 p-2 grid md:grid-cols-5 gap-8 pt-8'>
+					<div className='col-span-4'>
+						<h2>{projects.overview}</h2>
+						<p className='py-2'>
+							{projects.nftdrop1}
+						</p>
+						<p className='py-2'>
+							{projects.nftdrop2}
+						</p>
+						<button className='px-8 py-2 mt-4 mr-8 hover:scale-105 ease-in duration-300'>
+							<a
+								href='https://nft-drop-collections.vercel.app/'
+								target='_blank'
+								rel='noreferrer'
+							>
+								Demo
+							</a>
+						</button>
+						<button className='px-8 py-2 mt-4 hover:scale-105 ease-in duration-300'>
+							<a
+								href='https://github.com/Matiaspp96/nft-drop/'
+								target='_blank'
+								rel='noreferrer'
+							>
+								Code
+							</a>
+						</button>
+					</div>
+					<div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
+						<div className='p-2'>
+							<p className='text-center font-bold pb-2'>{projects.technologies}</p>
+							<div className='grid grid-cols-3 md:grid-cols-1'>
+								<p className='text-gray-600 py-2 flex items-center'>
+									<SiTypescript className='mr-2' /> TypeScript{' '}
+								</p>
+								<p className='text-gray-600 py-2 flex items-center'>
+									<SiNextdotjs className='mr-2' /> Next.js{' '}
+								</p>
+								<p className='text-gray-600 py-2 flex items-center'>
+									<SiEthereum className='mr-2' /> ThirdWeb{' '}
+								</p>
+								<p className='text-gray-600 py-2 flex items-center'>
+									<SiTailwindcss className='mr-2' /> Tailwind{' '}
+								</p>
+								<p className='text-gray-600 py-2 flex items-center'>
+									<SiVercel className='mr-2' /> Sanity{' '}
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default nftdrop;
+
+export async function getStaticProps({ locale }) {
+	const response = await import(`../lang/${locale}.json`)
+
+	return {
+		props: {
+			projects: response.projects,
+		}
+	}
+}
